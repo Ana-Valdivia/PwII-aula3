4e19c6a2205f2d57d4f76ed291852c7658f968c1
ana-valdivia <ana.raquel1291@hotmail.com>
2019-08-14 21:56:29 -0300

    utilização da função seta

--- impl/revisao.js
+++ impl/revisao.js
@@ -2,7 +2,8 @@ var n1 = 8;
 var n2 = 9;
 var n3 = 10;
 //let media = (n1 + n2 + n3)/3
-var fMedia = function (nota1, nota2, nota3) {
-    return (nota1 + nota2 + nota3) / 3;
-};
+//let fMedia = function(nota1:number,nota2:number,nota3:number):number{
+//return(nota1+nota2+nota3)/3
+//}
+var fMedia = function (nota1, nota2, nota3) { return (nota1 + nota2 + nota3) / 3; };
 console.log("Sua m\u00E9dia  \u00E9 : " + fMedia(n1, n2, n3));
--- revisao.ts
+++ revisao.ts
@@ -3,8 +3,10 @@
         let n2 : number = 9
         let n3 : number = 10
         //let media = (n1 + n2 + n3)/3
-        let fMedia = function(nota1:number,nota2:number,nota3:number):number{
-            return(nota1+nota2+nota3)/3
-        }
+        //let fMedia = function(nota1:number,nota2:number,nota3:number):number{
+            //return(nota1+nota2+nota3)/3
+        //}
+
+        let fMedia =(nota1:number,nota2:number,nota3:number) => (nota1+nota2+nota3)/3
         console.log(`Sua média  é : ${fMedia(n1,n2,n3)}`)  
     
