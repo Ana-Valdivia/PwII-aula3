fc445fbbf8b03acdbb06471a054a9e34ab8468e6
ana-valdivia <ana.raquel1291@hotmail.com>
2019-08-14 22:31:33 -0300

    Exercicio Volumes Caixa

---
+++ exercicio.ts
@@ -0,0 +1,8 @@
+let largura = 4
+let comprimento =12
+let altura =7
+let vol =function(valor1:number,valor2:number,valor3:number):number{
+    return(valor1*valor2*valor3)
+} 
+
+console.log(`O volume é ${vol(largura,comprimento,altura)}`)
---
+++ impl/exercicio.js
@@ -0,0 +1,7 @@
+var largura = 4;
+var comprimento = 12;
+var altura = 7;
+var vol = function (valor1, valor2, valor3) {
+    return (valor1 * valor2 * valor3);
+};
+console.log("O volume \u00E9 " + vol(largura, comprimento, altura));
