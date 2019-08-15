b25181534a5ef12b5468ba763cfd2bf54f53b2da
ana-valdivia <ana.raquel1291@hotmail.com>
2019-08-14 21:45:58 -0300

    criação de funções

--- impl/revisao.js
+++ impl/revisao.js
@@ -1,5 +1,8 @@
-var n1 = 10;
+var n1 = 8;
 var n2 = 9;
-var n3 = 5;
-var media = (n1 + n2 + n3) / 3;
-console.log("Sua média  é " + media);
+var n3 = 10;
+//let media = (n1 + n2 + n3)/3
+var fMedia = function (nota1, nota2, nota3) {
+    return (nota1 + nota2 + nota3) / 3;
+};
+console.log("Sua m\u00E9dia  \u00E9 : " + fMedia(n1, n2, n3));
--- revisao.ts
+++ revisao.ts
@@ -1,7 +1,10 @@
     
-        let n1 : number = 10
+        let n1 : number = 8
         let n2 : number = 9
-        let n3 : number = 5
-        let media = (n1 + n2 + n3)/3
-        console.log("Sua média  é " + media)  
+        let n3 : number = 10
+        //let media = (n1 + n2 + n3)/3
+        let fMedia = function(nota1:number,nota2:number,nota3:number):number{
+            return(nota1+nota2+nota3)/3
+        }
+        console.log(`Sua média  é : ${fMedia(n1,n2,n3)}`)  
     
